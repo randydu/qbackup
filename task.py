4e01e4cb70bf61d55c9db2d2a5acf0929ce9a43a
randy <randydu@gmail.com>
2020-04-24 11:25:59 -0700

    init

---
+++ task.py
@@ -0,0 +1,18 @@
+"""
+Task
+"""
+
+class Task(object):
+    pass
+
+class SingleFileTask(Task):
+    """ copy a single file """
+    def __init__(self):
+        pass
+
+class SingleDirTask(Task):
+    """ copy a single dir """
+    def __init__(self):
+        pass
+
+
